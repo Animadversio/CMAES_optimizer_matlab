f8bd7a48b8d2f6500d5f45f124652a55299428a0
Animadversio <binxu.wang@wustl.edu>
2020-03-01 16:29:50 -0600

    Finish Experiment system

--- printOptionStr.m
+++ printOptionStr.m
@@ -8,8 +8,10 @@ function fullstr = printOptionStr(options)
 %     fullstr = strcat(fullstr, opt_strs{Fi});
 % end
 % https://www.mathworks.com/matlabcentral/answers/335296-format-output-from-jsonencode-to-make-it-more-human-readable
-fullstr = jsonencode(options);
-fullstr = strrep(fullstr, ',', sprintf(',\r'));
+fullstr = jsonencode(options); % use jsonencode as a proxy for text representation of options
+fullstr = strrep(fullstr, ':', sprintf(': ')); % increase space after :
+fullstr = strrep(fullstr, ',', sprintf(',\r')); % change line after each , 
+fullstr = strrep(fullstr, '_', sprintf(' ')); % avoid `_` interpreted as Latex
 fullstr = strrep(fullstr, '{', sprintf(''));
 fullstr = strrep(fullstr, '}', sprintf(''));
 end
--- testing_GANoptim.m
+++ testing_GANoptim.m
@@ -1,5 +1,4 @@
 %% Testing Optimizers on Real GANs
-
 % declare net
 net = alexnet;
 pic_size = net.Layers(1).InputSize;
@@ -9,30 +8,33 @@ addpath(GANpath)
 G = FC6Generator('matlabGANfc6.mat');
 my_final_path =  '\\storage1.ris.wustl.edu\crponce\Active\Data-Computational\Project_Optimizers';
 %%
-% declare your number of generations
-n_gen = 100 ;
-unit = {"fc8", 2};
+n_gen = 100 ; % declare your number of generations
+unit = {"fc8", 2}; % Select target unit 
 % unit = {"conv2", 10, 100};
 
-options = struct("population_size",40, "select_cutoff",20, "lr",2, "mu",0.005, "Lambda",1, ...
-        "Hupdate_freq",201, "maximize",true, "max_norm",300, "rankweight",true, "rankbasis", false, "nat_grad",false);
-Optimizer = ZOHA_Sphere(4096, options);
-% options = struct("population_size",40, "select_cutoff",20, "lr_sph",2.5, "mu_sph",0.003, "lr_norm", 10, "mu_norm", 10, "Lambda",1, ...
-%         "Hupdate_freq",201, "maximize",true, "max_norm",400, "rankweight",true, "rankbasis", true, "nat_grad",false);
-% Optimizer = ZOHA_Cylind(4096, options);
+% Set options for optimizer (There is default value, so it can run with empty structure)
+% options = struct("population_size",40, "select_cutoff",20, "lr",2, "mu",0.005, "Lambda",1, ...
+%         "Hupdate_freq",201, "maximize",true, "max_norm",300, "rankweight",true, "rankbasis", false, "nat_grad",false);
+% Optimizer = ZOHA_Sphere(4096, options);
+options = struct("population_size",40, "select_cutoff",20, "lr_sph",2, "mu_sph",0.005, "lr_norm", 10, "mu_norm", 10, "Lambda",1, ...
+        "Hupdate_freq",201, "maximize",true, "max_norm",400, "rankweight",true, "rankbasis", true, "nat_grad",false);
+Optimizer = ZOHA_Cylind(4096, options);
+% Optimizer = CMAES_ReducDim(4096, [], 50);
+% Optimizer.getBasis("rand");
+% Optimizer =  CMAES_simple(4096, []);
 
+Visualize = true;
+SaveImg = false;   
+SaveData = true; 
+options = Optimizer.opts; % updates the default parameters
+options.Optimizer = class(Optimizer);
+%%
+% define random set of input vectors (samples from a gaussian, 0, -1)
+% 30 x 4096
+init_genes = normrnd(0,1,30,4096);% have to make sure the first row cannot be all 0. 
+% normrnd(0,1,30,4096) * 9.04;
 % genes = normrnd(0,1,30,4096) * 9.04;
 % genes = [mean(genes, 1) ; genes]; % have to make sure the first row cannot be all 0. 
-% Optimizer = CMAES_ReducDim(genes, [], 50);
-% Optimizer.getBasis("rand");
-
-% Optimizer =  CMAES_simple(genes, []);
-Visualize = true;
-Save = false;   
-
-%
-init_genes = normrnd(0,1,30,4096)*4;
-init_genes = [init_genes]; % have to make sure the first row cannot be all 0. 
 fign = [];
 if ~isempty(fign)
     h = figure(fign);
@@ -41,13 +43,16 @@ else
     h = figure();
     h.Position = [210         276        1201         645];
 end
+% Annotate the readable parameters on the figure. 
+annotation(h,'textbox',...
+    [0.485 0.467 0.154 0.50],'String',split(printOptionStr(options),','),...
+    'FontSize',14,'FitBoxToText','on','EdgeColor','none')
 % all_layers_wanted = {'conv4','conv5','conv3','conv2','conv1'};
 % n_unitsInChan = 10 ;
 % % select random units
 % rng(1)
 % all_units = randsample(nrows*ncols,n_unitsInChan,false) ;
-% t_unit = all_units(iUnit) ;
-
+% t_unit = all_units(iUnit) ; 
 my_layer = unit{1} ; 
 iChan = unit{2} ; 
 if contains(my_layer,"fc")
@@ -55,16 +60,17 @@ if contains(my_layer,"fc")
 else
 	t_unit = unit{3};
 end
-if ~exist(fullfile(my_final_path, my_layer, sprintf('%02d',t_unit) ),'dir')
-    mkdir(fullfile(my_final_path, my_layer, sprintf('%02d',t_unit) ))
+exp_dir = fullfile(my_final_path, sprintf('%s_%d_%d',my_layer, iChan, t_unit) );
+if ~exist(exp_dir,'dir')
+    mkdir(exp_dir)
 end
+fprintf(exp_dir)
+fprintf(printOptionStr(options))
 % get activation size
 act1 = activations(net,rand(pic_size),my_layer,'OutputAs','Channels');
 [nrows,ncols,nchans] = size(act1) ;
 [i,j] = ind2sub( [nrows ncols], t_unit ) ;
 %    evolutions
-% define random set of input vectors (samples from a gaussian, 0, -1)
-% 30 x 4096
 genes = init_genes; 
 codes_all = [];
 scores_all = [];
@@ -78,6 +84,7 @@ for iGen = 1:n_gen
     % get activations
     act1 = activations(net,pics,my_layer,'OutputAs','Channels');
     act_unit = squeeze( act1(i,j,iChan,:) ) ;
+    disp(act_unit')
     % Record info 
     scores_all = [scores_all; act_unit]; 
     codes_all = [codes_all; genes];
@@ -86,6 +93,7 @@ for iGen = 1:n_gen
     % save the new codes as 'genes'
     [genes_new,tids] = Optimizer.doScoring(genes, act_unit, true);
     if Visualize
+    set(0,"CurrentFigure",h)
     % plot firing rate as it goes
     subplot(2,2,1)
     mean_activation(iGen) = mean(act_unit) ;
@@ -125,23 +133,25 @@ for iGen = 1:n_gen
     axis image off
     drawnow
 	end
-	if Save
-    image_name = sprintf('%s_%03d_%02d_%02d_%02d_%02d.jpg',my_layer,iChan,t_unit,nrows,ncols,iGen) ;
-    imwrite( meanPic ,  ...
-        fullfile(my_final_path, my_layer, sprintf('%02d',t_unit), image_name ) , 'jpg')
+	if SaveImg
+        image_name = sprintf('%s_%03d_%02d_%02d_%02d_%02d.jpg',my_layer,iChan,t_unit,nrows,ncols,iGen) ;
+        imwrite( meanPic ,  ...
+            fullfile(my_final_path, my_layer, sprintf('%02d',t_unit), image_name ) , 'jpg')
     end
     genes = genes_new;
 end % of iGen
 norm_all = sqrt(sum(codes_all.^2,2));
-%%
-save(fullfile(my_final_path, my_layer, sprintf('%02d',t_unit)), "scores_all","codes_all","generations")
-%save(fullfile(my_final_path, my_layer, sprintf('%02d',t_unit), image_name(1:end-4)) ,'mean_activation')
-%%
-structString = structfun(@(field)sprintf(field),s,'UniformOutput',false);
-cellstr = struct2cell(structString);
-str = sprintf('\n%s',cellstr{:})
+%
+exp_id = randi(9999,1);
+if SaveData % write the parametes strings to file.
+    fid = fopen(fullfile(exp_dir, sprintf('parameter_%s_tr%04d.txt', class(Optimizer), exp_id)), 'wt');
+    fprintf(fid, printOptionStr(options));
+    fprintf("\n");
+    fclose(fid);
+    save(fullfile(exp_dir, sprintf("Evol_Data_%s_tr%04d.mat", class(Optimizer), exp_id)), "scores_all","codes_all","generations","norm_all")
+    saveas(h, fullfile(exp_dir, sprintf("Evol_trace_%s_tr%04d.png", class(Optimizer), exp_id)))
+end
 %%
 [~,idx]=max(scores_all);
 imwrite( G.visualize(codes_all(idx, :)), sprintf('%s_%02d.jpg',my_layer, iChan) , 'jpg')
-%%
 BestImg = G.visualize(codes_all(idx, :));
