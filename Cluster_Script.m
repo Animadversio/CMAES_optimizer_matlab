8fc07bfdf5cb08dc79fdbc287ec357fba07c7924
Animadversio <binxu.wang@wustl.edu>
2020-03-16 13:09:48 -0500

    Update Cluster_Script.m

--- Cluster_Script.m
+++ Cluster_Script.m
@@ -7,3 +7,23 @@ c.AdditionalProperties.MemUsage = '4000';
 c.AdditionalProperties.QueueName = 'dque';    
 c.AdditionalProperties.WallTime = '01:00:00';
 c.saveProfile
+% Run a batch script, capturing the diary, adding a path to the workers
+% and transferring some required files.
+j = batch(myCluster, 'script1', ...
+          'AdditionalPaths', '\\Shared\Project1\HelperFiles',...
+          'AttachedFiles', {'script1helper1', 'script1helper2'});
+j = c.batch(@parallel_example, 1, {}, 'Pool', 8, ...
+        'CurrentFolder','.', 'AutoAddClientPath',false);
+% j = batch(cluster, fcn, N, {x1,..., xn}) 
+%     runs the function specified by
+%     a function handle or function name, fcn, on a worker using the
+%     identified cluster.  The function returns j, a handle to the job object
+%     that runs the function. The function is evaluated with the given
+%     arguments, x1,...,xn, returning N output arguments.  The function file
+%     for fcn is copied to the worker. 
+wait(j)
+diary(j,"mydiary.log")
+id = j.ID;
+clear j
+j = c.findJob('ID', 4);
+j.fetchOutputs{:};
