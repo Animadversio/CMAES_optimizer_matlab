d8bd7d67dcc525e368e89a3b1c465232f8dc726d
Binxu Wang <binxu.wang@wustl.edu>
2020-04-27 11:52:54 -0500

    adding varargin to norm_axis

--- geomtry_util/norm_axis.m
+++ geomtry_util/norm_axis.m
@@ -1,3 +1,8 @@
-function norms = norm_axis(Vecs, dim)
+function norms = norm_axis(Vecs, varargin)
+if nargin < 2
+    dim = 1 ;
+else
+    dim = varargin{1};
+end
 norms = sqrt(sum(Vecs.^2,dim));
 end
